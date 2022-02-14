c9d764f2a0c44fb780fc714bf5ad6e62a241508b
joker-more <joker.more976@gmail.com>
2022-02-15 00:30:59 +0900

    add header

---
+++ product/inc/fixedpoint8.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <cstdint>
+#include "overflowdetect.h"
+
+/**
+ * @brief Class for handling fixed points.
+ * 
+ */
+
+namespace fixedpoints {
+    const int8_t DecimalPointBit8 = 8;
+    class fixedpoint8{
+        private:
+            int64_t _fixedpoint;
+
+        public:
+            fixedpoint8();
+            fixedpoint8(int64_t fixedpoint);
+            fixedpoint8(double floatingpoint);
+            ~fixedpoint8();
+            int64_t getfixedpoint();
+            fixedpoint8 operator+ (fixedpoint8& op);
+            fixedpoint8 operator- (fixedpoint8& op);
+            fixedpoint8 operator* (fixedpoint8& op);
+            fixedpoint8 operator/ (fixedpoint8& op);
+            bool operator== (fixedpoint8& op);
+            bool operator!= (fixedpoint8& op);
+    };
+}
